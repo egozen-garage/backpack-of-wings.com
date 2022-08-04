d74580c7ea31c6d05a333e322c3006e1daa65960
armin <armin.arndt@googlemail.com>
2022-08-04 18:39:50 +0200

    connect GPS dots with lines

--- src/utilities/MovebankApi.js
+++ src/utilities/MovebankApi.js
@@ -32,8 +32,10 @@ function MovebankAPI() {
 
 
 let height = 800;
-let width = 2000;
-let projection = d3.geoNaturalEarth1();
+let width = 1200;
+let height2 = '100vh';
+let width2 = '100vw';
+let projection = d3.geoEquirectangular().scale(1000000).translate([width/2, height/2]);
 
 
 class DrawSVG extends React.Component { 
@@ -46,35 +48,93 @@ class DrawSVG extends React.Component {
     
     
     componentDidMount(){
-        let accessToRef = d3.select(this.myRef.current);
-        // accessToRef.style("background-color", "red")
-        accessToRef.attr("viewBox", [0, 0, width, height]);
+        // select SVG tag -> see render()
+        let svg = d3.select(this.myRef.current);
+
+        // define style and size of SVG tag
+        svg
+            // .attr("viewBox", [0, 0, width, height])
+            .attr('width', width2)
+            .attr('height', height2)
+            .attr('viewBox','0 0 '+Math.min(width2,height2) +' '+Math.min(width2,height2) )
+            // .attr("transform", "translate(" + Math.min(width,height) / 2 + "," + Math.min(width,height) / 2 + ")")
+            .style('background-color', '#7FFFD4');
         
         
         let lastItemCount = this.props.birdData.length-1
         let current_latitude = this.props.birdData[lastItemCount].location_lat
         let current_longitude = this.props.birdData[lastItemCount].location_long
-        console.log("last entry: " + this.props.birdData[lastItemCount].timestamp)
+        console.log("last entry: " + lastItemCount + this.props.birdData[lastItemCount].timestamp)
+
+        // center position of geo view
+        projection.center([current_latitude, current_longitude]);
 
 
 
-        accessToRef.append("g")
+
+        // draw LINES
+        // convert API data to a array --> each item of the array should have 2 GPS locations (start and end) to draw a line
+        var data = this.props.birdData;
+        var linesArray = [];
+        // for(var i in data){
+        for(var i = 0; i < lastItemCount; i++){
+            var nextCount = i + 1;
+            // console.log('nextCount = ', typeof nextCount + nextCount +" / i = " +  typeof i + i);
+            var source = [+data[i].location_lat, +data[i].location_long];
+            var target = [+data[nextCount].location_lat, +data[nextCount].location_long];
+            let topush = {type: "LineString", coordinates: [source, target]}
+            linesArray.push(topush)
+        }
+        console.log('line data array', linesArray);
+        // A path generator
+        const path = d3.geoPath()
+            .projection(projection)
+        // Add the path
+        svg.append("g").attr('class','lines').selectAll("myPath")
+            .data(linesArray)
+            .enter()
+            .append("path")
+                .attr("d", function(d){ 
+                    // let line = "M" + d.coordinates[0][0]  + " " +  d.coordinates[0][1] + " L" + d.coordinates[1][0] + " " +  d.coordinates[1][1]
+                    // console.log('create curvy path: ' + line)
+                    // return path(line)
+                    return path(d)
+                })
+                .style("fill", "none")
+                .style("stroke", "orange")
+                .style("stroke-width", 1)
+
+
+
+
+
+
+
+
+
+        // draw circle for each entry in birdData
+        svg.append("g").attr('class','circles')
             .selectAll("circle")
             // .data(sampleData.locations)
             .data(this.props.birdData)
             .join("circle")
             .attr("transform", d => `translate(${projection([d.location_lat, d.location_long])})`)
-            .attr("r", 1.5)
+            .attr("r", 2)
             .append("title")
             .text(d => "time stamp: " + d.timestamp + " latitude: " + d.location_lat + " longitude: " + d.location_long);
         
-        d3.selectAll("circle:last-child")
+        // style of current position (last circle of birdData)
+        svg.selectAll("circle:last-child").attr('class','last-circle')
             .attr("r", 10.5)
             .style("fill", "#69b3a2")
             .append("title").text(d => "last location");
 
-        projection.scale(1000000).translate([width/2, height/2]);
-        projection.center([current_latitude, current_longitude]) 
+        
+
+
+
+
+
 
     }
 
