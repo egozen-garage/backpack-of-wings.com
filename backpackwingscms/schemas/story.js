131e1552978e950457962f597a8cb06ab76ba558
Ken Rodenwaldt <kennyro@me.com>
2022-10-18 19:34:06 +0200

    Sanity > add and configure schemas

--- backpackwingscms/schemas/story.js
+++ backpackwingscms/schemas/story.js
@@ -1,41 +1,52 @@
 export default {
-    name: 'story',
-    title: 'Story',
-    type: 'document',
-    fields: [
-      {
-        name: 'name',
-        title: 'Name',
-        type: 'string',
-      },
-      {
-        name: "email",
-        title: "Email",
-        type: "string",
-      },
-      {
-        name: "message",
-        title: "Message",
-        type: "text",
-      },
-      {
-        name: 'publishedAt',
-        title: 'Published at',
-        type: 'datetime',
-        readOnly: true,
-      },
-    ],
-  
-    preview: {
-      select: {
-        title: 'name',
-      },
-      prepare(selection) {
-        const {name} = selection
-        return Object.assign({}, selection, {
-          subtitle: name && `by ${name}`,
-        })
-      },
+  name: "story",
+  title: "Story",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
     },
-  }
-  
+    {
+      name: "country",
+      title: "Country",
+      type: "reference",
+      to: [{ type: "category" }],
+      options: { filter: "!defined(country)" },
+    },
+    {
+      name: "location",
+      title: "Location Name",
+      type: "string",
+    },
+    {
+      name: "email",
+      title: "Email",
+      type: "string",
+    },
+    {
+      name: "message",
+      title: "Message",
+      type: "text",
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+      readOnly: true,
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "name",
+    },
+    prepare(selection) {
+      const { name } = selection;
+      return Object.assign({}, selection, {
+        subtitle: name && `by ${name}`,
+      });
+    },
+  },
+};
---
+++ backpackwingscms/schemas/youtube.js
@@ -0,0 +1,17 @@
+export default {
+  name: "youtube",
+  type: "object",
+  title: "Youtube Embed",
+  fields: [
+    {
+      name: "url",
+      type: "url",
+      title: "URL",
+    },
+  ],
+  preview: {
+    select: {
+      url: "url",
+    },
+  },
+};
