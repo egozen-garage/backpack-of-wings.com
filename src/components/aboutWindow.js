1b7b0d51f0197afda75217491a61aaf2bbee472a
Ken Rodenwaldt <kennyro@me.com>
2022-11-08 19:34:45 +0100

    Dashboard Page > created animated collapsible window with useState + useSpring

--- src/components/aboutWindow.js
+++ src/components/aboutWindow.js
@@ -1,22 +1,45 @@
 import React from "react";
 import { About } from "./About";
 import { useState } from "react";
-import '../css/gradientAnimation.css';
+import { useSpring, animated } from "react-spring";
+import styled from "styled-components";
+import "../css/gradientAnimation.css";
 
 export default function AboutWindow() {
-//   const [aboutPopup, setAboutPopup] = useState(false);
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleCollapsible = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+  const AboutAnimatedStyle = useSpring({
+    height: isOpen ? "100vh" : "0",
+  });
+  const PanelContent = styled(animated.div)`
+    color: black;
+    overflow: scroll;
+    z-index: 35;
+  `;
 
   return (
     <>
-    <div className="z-45 aboutBackground gradientDashboard fixed top-0 h-36 w-full">
-      <div onClick={() => setIsOpen(!isOpen)} className="aboutContainer bg-white fixed z-40 rounded-2xl top-10 left-20 p-2">
-        <button className="toggle">
-          <h1 className="text-xl mx-3">Backpack of Wings: Sensory Networks</h1>
-        </button>
+      <div className="z-45 aboutBackground fixed top-0">
+        <div
+          onClick={toggleCollapsible}
+          className="aboutContainer bg-white fixed z-40 rounded-2xl top-10 left-20 p-2"
+        >
+          <button className="toggle">
+            <h1 className="text-xl mx-3">
+              Backpack of Wings: Sensory Networks
+            </h1>
+          </button>
+        </div>
       </div>
-    </div>
-    {isOpen && <About></About>}
+      <div className="z-40 gradientDashboard fixed h-36 w-full"></div>
+      <PanelContent
+        style={AboutAnimatedStyle}
+        className="gradientDashboard fixed w-full"
+      >
+        <About />
+      </PanelContent>
     </>
   );
 }
