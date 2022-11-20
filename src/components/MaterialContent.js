cdcf41f88743ae1a6b1e8d23922657d2d686be33
Ken Rodenwaldt <kennyro@me.com>
2022-11-20 16:55:08 +0100

    added new component MaterialContent.js

---
+++ src/components/MaterialContent.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { getImageDimensions } from '@sanity/asset-utils'
+import urlBuilder from "@sanity/image-url";
+
+export function MaterialContent(props) {
+    let singleLandmarkData = props.singleLandmarkData;
+    const blockImageComponent = ({value, isInline}) => {
+        const {width, height} = getImageDimensions(value)
+        return (
+          <img 
+            src={urlBuilder()
+            .image(value)
+            .width(isInline ? 100 : 800)
+            .fit('max')
+            .auto('format')
+            .url()}
+          /> 
+        )
+      }
+    const blockComponents = {
+        types: {
+          // ISSUE: STOPS TO RENDER RIGHT HERE, PROBLEM WITH PORTABLE TEXT
+          image: blockImageComponent,
+        },
+      }
+
+    console.log("MaterialContent = " + singleLandmarkData)
+
+
+  return (
+    <>
+    {singleLandmarkData.materialArray.map(
+        ({
+          _type,
+          embedMapsSRC,
+          embedYoutubeHTML,
+          url,
+          blockContent,
+          tweetEmbed,
+          embedFacebookHTML,
+          embedTiktokHTML,
+        }) => (
+          <div className="bg-white rounded-2xl w-80 h-[200px] mx-6 shrink-0 p-6 overflow-scroll">
+            {_type === "googleMaps" ? (
+              <iframe src={embedMapsSRC}></iframe>
+            ) : _type === "youtube" ? (
+              <iframe src={embedYoutubeHTML}></iframe>
+            ) : _type === "imageURL" ? (
+              <img src={url} />
+            ) : _type === "image" ? (
+              // ISSUE: URL ITEM NOT READ
+              <img src={_type} />
+            ) : _type === "blockObj" ? (
+              <>
+              {blockContent.map( ({_type, asset}) => (
+                <p>{_type}</p>
+              ))}
+              {console.log("MaterialContent, array of portable text blocks =" + JSON.stringify(blockContent, null, 2))}
+              {console.log("MaterialContent, optional object of custom components to use=" + JSON.stringify(blockComponents, null, 2))}
+              </>
+              // <>
+              // <PortableText
+              //   value={blockContent}
+              //   components={blockComponents}
+              // /> 
+              // </>
+              ) : _type === "twitter" ? (
+              <iframe
+                border="0"
+                frameborder="0"
+                height="250"
+                width="550"
+                src={tweetEmbed}
+              ></iframe>
+            ) : _type === "facebook" ? (
+              <iframe src={embedFacebookHTML}></iframe>
+            ) : _type === "tiktok" ? (
+              <iframe src={embedTiktokHTML}></iframe>
+            ) : (
+              <div>other Material</div>
+            )}
+          </div>
+        )
+      )}
+  </>
+  );
+}
--- src/components/StoryCategory.js
+++ src/components/StoryCategory.js
@@ -9,80 +9,65 @@ import PortableText from "react-portable-text";
 import CallSanityAPI from "../utilities/CallSanityAPI";
 import "../css/gradientAnimation.css";
 import "../css/animation.css";
+import urlBuilder from "@sanity/image-url";
+import { sanityClient } from "@sanity/client";
+import { getImageDimensions } from '@sanity/asset-utils'
+import { MaterialContent } from "./MaterialContent.js";
+
+// const urlFor = (source) =>
+//   urlBuilder({ projectId: "qw7xv7xh", dataset: "production" }).image(source);
+
+// const client = sanityClient({
+//   projectId: "qw7xv7xh",
+//   dataset: "production",
+//   apiVersion: "2022-10-14",
+//   useCdn: true,
+// });
+
+// const serializer = {
+//   types: {
+//     image: (props) => (
+//       <pre>{props}</pre>
+//       // <img className="blockIMG" src={urlFor(props)}/>
+//     ),
+//   },
+// };
 
 export function StoryCategory(props) {
   const { landmark } = useParams();
   // const location = useLocation();
   // let currentLandmark = location.pathname.split("/")[2];
   // let currentLandmark = landmark;
-  
+
   // let istanbul = landmarkData[0]
   // let dudaimsite = landmarkData[1]
   // let hama = landmarkData[2]
   // let lackova = landmarkData[3]
   // let droemling = landmarkData[4]
   // let neveeitan = landmarkData[5]
-  
-  let landmarkData = props.landmarkData
+
+  let landmarkData = props.landmarkData;
   const data = () => {
     let tempData;
-    for(const x in landmarkData){
-      if(landmark === landmarkData[x].url.current){
-        tempData = landmarkData[x]
+    for (const x in landmarkData) {
+      if (landmark === landmarkData[x].url.current) {
+        tempData = landmarkData[x];
       }
     }
-    return tempData
-  }
-  const singleLandmarkData = data()
-  // console.log("storyCatergory > landmark data : " + JSON.stringify(singleLandmarkData) )
-  
-  // let queryLandmark = CallSanityAPI(`*[_type == "landmark" && url.current == "${landmark}"]`);
-  // let landmarkContent = queryLandmark.data[0];
-  
-  // console.log("storyCategory > current Landmark:" + currentLandmark)
-  // console.log("storyCatergory > landmark data: " + landmarkData[0].url.current )
-  // console.log("storyCatergory > materials of landmark : " + JSON.stringify(hama.materialArray) )
-
-
+    return tempData;
+  };
+  const singleLandmarkData = data();
+  console.log("storyCatergory > landmark data : " + JSON.stringify(singleLandmarkData, null, 2) )
 
   return (
-      <div className="uploadStories-Wrapper uploadStoriesContainerAnimation fixed flex flex-col top-0 pt-4 z-30 right-0 h-screen w-[50rem] wideScreen:w-[80rem]">
-
-        {/* STORIES MATERIAL CONTAINER */}
-        <div className="gradientMaterialOverlay my-10 pl-[0.4rem]">
-          <div className="flex overflow-x-scroll scrollbar-hide">
-
-            {
-              singleLandmarkData ?
-              (
-                <>
-                  {singleLandmarkData.materialArray.map(({ _type, embedMapsSRC, embedYoutubeHTML, url, blockContent, tweetEmbed, embedFacebookHTML, embedTiktokHTML }) => (
-                    <div className="bg-white rounded-2xl w-80 h-[200px] mx-6 shrink-0 p-6">
-                      {_type === "googleMaps" ? 
-                        (<iframe src={embedMapsSRC}></iframe>) : 
-                      _type === "youtube" ?
-                        (<iframe src={embedYoutubeHTML}></iframe>) :
-                      _type === "imageURL" ?
-                        (<img src={url}/>) :
-                      _type === "image" ?
-                        (<img src={_type}/>) :
-                      // _type === "blockObj" ?
-                      //   (<PortableText content={blockContent}/>) :
-                      _type === "twitter" ?
-                        (<iframe border="0" frameborder="0" height="250" width="550" src={tweetEmbed}></iframe>) :
-                      _type === "facebook" ?
-                        (<iframe src={embedFacebookHTML}></iframe>) :
-                      _type === "tiktok" ?
-                        (<iframe src={embedTiktokHTML}></iframe>) :
-                        (<div>other Material</div>)
-                      } 
-                    </div>
-                  ))}
-                </>
-              ) : "" 
-            }
-
-            {/* {
+    <div className="uploadStories-Wrapper uploadStoriesContainerAnimation fixed flex flex-col top-0 pt-4 z-30 right-0 h-screen w-[50rem] wideScreen:w-[80rem]">
+      {/* STORIES MATERIAL CONTAINER */}
+      <div className="gradientMaterialOverlay my-10 pl-[0.4rem]">
+        <div className="flex overflow-x-scroll scrollbar-hide">
+          {singleLandmarkData ? (
+            <MaterialContent singleLandmarkData={singleLandmarkData}/>
+          ) : ("")}
+          {/* {
               currentLandmark === "istanbul" ?
               (
                 <>
@@ -253,12 +238,12 @@ export function StoryCategory(props) {
                   ))}
               </>)
             } */}
-          </div>
         </div>
-
-        {/* STORIES INPUT TEXT CONTAINER */}
-        <StoryInputForm currentLandmark={landmark}/>
-        {/* <Story /> */}
       </div>
+
+      {/* STORIES INPUT TEXT CONTAINER */}
+      <StoryInputForm currentLandmark={landmark} />
+      {/* <Story /> */}
+    </div>
   );
 }
