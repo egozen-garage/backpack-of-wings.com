d8d9823caf065d1644cc7d6636292aef839234a1
armin <armin.arndt@googlemail.com>
2022-11-23 09:25:31 +0100

    prepare mouse move timeout

--- src/components/mouseSleeper/index.js
+++ src/components/mouseSleeper/index.js
@@ -1,6 +1,47 @@
-
+// import { useRef, useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
 export function MouseSleeper(){
+    // const navigate = useNavigate()
+    // const [showMessage, setShowMessage] = useState(false)
+    // const timeUntilMessage = 5000
+    // const timeUntilReload = 2000
+    // var timeout;
+    // var timerReload =useRef();
+    // document.onmousemove = function(){
+    //     clearTimeout(timeout);
+    //     clearTimeout(timerReload.current);
+    //     setShowMessage(false)
+    //     timeout = setTimeout(function(){
+    //         // alert("move your mouse");
+    //         console.log("MouseSleeper - move you mouse")
+    //         setShowMessage(true)
+    //         StartReloadCountdown()
+    //         clearTimeout(timeout);
+    //     }, timeUntilMessage);
+    // }
+    // const StartReloadCountdown = () => {
+    //     timerReload.current = setTimeout(() => {
+    //         if(showMessage){ 
+    //         console.log("MouseSleeper - reload")
 
-    return
+    //         // navigate('/')
+    //         // window.location.reload()
+    //         }
+    //     },timeUntilReload)
+    // }
+    
+    // const message = (
+    //     <div className="fixed flex h-full z-60 top-0 bottom-0 right-0 left-0 items-center justify-center">
+    //         <div className="w-50 h-60">
+    //             Move you mouse to stay connected
+    //         </div>
+    //     </div>
+    // )
+
+    // return(
+    //     <>
+    //         { showMessage ? message : ""}
+    //     </>
+    // )
 }
